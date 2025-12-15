5b72ccafb07ed299a75e144eebafa7730b3f3c39
yuhang <yuhang@x.ai>
2025-12-15 21:53:06 +0000

    Update SkyStarBackground and project configuration

--- touchtime/Components/SkyStarBackground.swift
+++ touchtime/Components/SkyStarBackground.swift
@@ -10,20 +10,6 @@ import SwiftUI
 // Star particle view for night sky
 struct StarParticle: View {
     let size: CGFloat
-    let twinkleDelay: Double
-    let twinkleDuration: Double
-    @State private var isTwinkling = false
-    
-    // Calculate target opacity based on star size
-    private var targetOpacity: Double {
-        if size > 1.5 {
-            return 0.3  // Bright stars twinkle to 30% opacity
-        } else if size > 0.8 {
-            return 0.2  // Medium stars twinkle to 20% opacity
-        } else {
-            return 0.1  // Small stars twinkle to 10% opacity
-        }
-    }
     
     var body: some View {
         Circle()
@@ -34,36 +20,22 @@ struct StarParticle: View {
                 Color(white: 0.95, opacity: 1.0)  // Smaller stars slightly dimmer
             )
             .frame(width: size, height: size)
-            .opacity(isTwinkling ? targetOpacity : 1.0)
             .blur(radius: size > 1.5 ? 0.3 : 0)
-            .shadow(color: Color(white: 0.9).opacity(isTwinkling ? 0.3 : 0.9), radius: size > 1.2 ? 3 : 1)  // Dynamic glow
-            .animation(
-                Animation.spring(duration: twinkleDuration)
-                    .repeatForever(autoreverses: true)
-                    .delay(twinkleDelay),
-                value: isTwinkling
-            )
-            .onAppear {
-                isTwinkling = true
-            }
+            .shadow(color: Color(white: 0.9).opacity(0.9), radius: size > 1.2 ? 3 : 1)
     }
 }
 
 // Container for multiple stars
 struct StarsView: View {
     var starCount: Int = 50  // Number of stars (configurable)
-    @State private var stars: [(id: Int, x: CGFloat, y: CGFloat, size: CGFloat, twinkleDelay: Double, twinkleDuration: Double)] = []
+    @State private var stars: [(id: Int, x: CGFloat, y: CGFloat, size: CGFloat)] = []
     
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 ForEach(stars, id: \.id) { star in
-                    StarParticle(
-                        size: star.size,
-                        twinkleDelay: star.twinkleDelay,
-                        twinkleDuration: star.twinkleDuration
-                    )
-                    .position(x: star.x, y: star.y)
+                    StarParticle(size: star.size)
+                        .position(x: star.x, y: star.y)
                 }
             }
             .onAppear {
@@ -81,32 +53,26 @@ struct StarsView: View {
     }
     
     private func generateStars(in size: CGSize) {
-        var newStars: [(id: Int, x: CGFloat, y: CGFloat, size: CGFloat, twinkleDelay: Double, twinkleDuration: Double)] = []
+        var newStars: [(id: Int, x: CGFloat, y: CGFloat, size: CGFloat)] = []
         
         for i in 0..<starCount {
             // Create different star types
             let starType = Double.random(in: 0...1)
             let starSize: CGFloat
-            let twinkleDuration: Double
             
             if starType < 0.75 {  // 75% small dim stars
                 starSize = CGFloat.random(in: 0.4...0.8)
-                twinkleDuration = Double.random(in: 1.0...2.0)  // Faster twinkle for more noticeable effect
             } else if starType < 0.97 {  // 22% medium stars
                 starSize = CGFloat.random(in: 0.8...1.4)
-                twinkleDuration = Double.random(in: 0.8...1.8)  // Faster twinkle
             } else {  // 3% bright stars
-                starSize = CGFloat.random(in: 1.5...2.5)  // Bigger bright stars
-                twinkleDuration = Double.random(in: 0.6...1.5)  // Fastest twinkle for bright stars
+                starSize = CGFloat.random(in: 1.5...2.5)
             }
             
             newStars.append((
                 id: i,
                 x: CGFloat.random(in: 0...size.width),
                 y: CGFloat.random(in: 0...size.height),
-                size: starSize,
-                twinkleDelay: Double.random(in: 0...3),
-                twinkleDuration: twinkleDuration
+                size: starSize
             ))
         }
         stars = newStars
