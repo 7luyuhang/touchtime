da0ea0d3d848e037ff0bdee495ca5320f738ef09
yuhang <7luyuhang@gmail.com>
2026-03-21 15:07:15 +0000

    updated

--- touchtime/Views/SubView/SetAlarmSheet.swift
+++ touchtime/Views/SubView/SetAlarmSheet.swift
@@ -168,7 +168,7 @@ struct SetAlarmSheet: View {
                                 .tint(.blue) // Toggle Colour
                             }
 
-                            if record.repeatRule == .weekly && record.isEnabled {
+                            if record.repeatRule == .weekly {
                                 repeatWeekdayRow(for: record)
                             }
                         }
