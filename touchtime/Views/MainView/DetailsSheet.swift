283aa4a7887b982da7676b15d8a3ca5214093f73
yuhang <7luyuhang@gmail.com>
2026-07-23 23:01:13 +0100

    Update DetailsSheet.swift

--- touchtime/Views/MainView/DetailsSheet.swift
+++ touchtime/Views/MainView/DetailsSheet.swift
@@ -574,10 +574,10 @@ struct SunriseSunsetSheet: View {
                             if amount > 0 {
                                 Capsule()
                                     .fill(.cyan)
-                                    .frame(height: max(4, CGFloat(amount / referenceAmount) * 18))
+                                    .frame(height: max(5, CGFloat(amount / referenceAmount) * 18))
                             }
                         }
-                        .frame(width: 4, height: 18)
+                        .frame(width: 5, height: 18)
                     }
                 }
 
